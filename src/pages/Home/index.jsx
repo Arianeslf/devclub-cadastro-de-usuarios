0d158463b8b99ff3ac5f16aabf6585127a05cd10
Ariane Silvestre <arianesilvetremaira73@gmail.com>
2026-05-15 19:48:59 -0300

    modified 3 files

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,15 +1,33 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import "./style.css";
 import Trash from "../../assets/trash.svg";
 import api from "../../services/api";
 
 function Home() {
-const [users, setUsers] = useState([]);
+  const [users, setUsers] = useState([]);
+
+  const nameInputRef = useRef();
+  const ageInputRef = useRef();
+  const emailInputRef = useRef();
 
   async function getUsers() {
     const userFromApi = await api.get("/usuarios");
-    
     setUsers(userFromApi.data);
+  }
+
+  async function createUsers() {
+    await api.post("/usuarios", {
+      name: nameInputRef.current.value,
+      age: ageInputRef.current.value,
+      email: emailInputRef.current.value,
+    });
+
+    await getUsers();
+  }
+
+  async function deleteUsers(id) {
+    await api.delete(`/usuarios/${id}`);
+    await getUsers();
   }
 
   useEffect(() => {
@@ -20,10 +38,22 @@ const [users, setUsers] = useState([]);
     <div className="container">
       <form>
         <h1>Cadastro de Usuário</h1>
-        <input type="text" placeholder="Nome" name="name" />
-        <input type="number" placeholder="Idade" name="idade" />
-        <input type="email" placeholder="Email" name="email" />
-        <button type="submit">Cadastrar</button>
+        <input type="text" placeholder="Nome" name="name" ref={nameInputRef} />
+        <input
+          type="number"
+          placeholder="Idade"
+          name="idade"
+          ref={ageInputRef}
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          name="email"
+          ref={emailInputRef}
+        />
+        <button type="button" onClick={createUsers}>
+          Cadastrar
+        </button>
       </form>
       {users.map((user) => (
         <div key={user.id} className="user-card">
@@ -38,7 +68,7 @@ const [users, setUsers] = useState([]);
               Email: <span>{user.email}</span>
             </p>
           </div>
-          <button>
+          <button onClick={() => deleteUsers(user.id)}>
             <img src={Trash} alt="Excluir" />
           </button>
         </div>
